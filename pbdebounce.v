93dd9aba0ba4f21240cdef273d477ecdf822ed30
Pang Xinglei <143163356+wildfire322@users.noreply.github.com>
2024-05-03 22:23:28 +0800

    Add files via upload
    防抖动模块

---
+++ pbdebounce.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/04/13 15:36:13
+// Design Name: 
+// Module Name: pbdebounce
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pbdebounce(
+    input wire clk,
+    input wire button, 
+    output reg pbreg
+    );
+
+    reg [7:0] pbshift;
+
+    always@(posedge clk) begin
+        pbshift = pbshift<<1;
+        pbshift[0] = button;
+        if (pbshift==8'b0)
+            pbreg=0;
+        if (pbshift==8'hFF)
+            pbreg=1;    
+    end
+
+endmodule
+
