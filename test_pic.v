485a2adc390bbc0d71130b49f79c4ce5df7e4ca3
cyx2015s <miracle041206@163.com>
2024-06-09 16:44:49 +0800

    或许可用的vga测试模块

---
+++ test_pic.v
@@ -0,0 +1,14 @@
+module vga_test(input vga_clk,
+                input vga_rst,
+                input x_pos,
+                input y_pos,
+                output [11:0] pixel_data); // 蓝蓝蓝蓝绿绿绿绿红红红红
+    always (@posedge vga_clk or negedge vga_rst) begin
+        if (~vga_rst) begin
+            pixel_data <= 0;
+        end
+        else begin
+            pixel_data <= {x_pos [3:0], y_pos [3:0], 4'b0};
+        end
+    end
+endmodule
--- vga_test_top.v
+++ vga_test_top.v
@@ -1,45 +1,47 @@
-module VGA(
-    input clk,
-    input rst,
-    input [11:0] data_in, // 蓝蓝蓝蓝绿绿绿绿红红红红
-    output reg [8:0] row, // 480 < 512
-    output reg [9:0] col, // 640 < 1024
-    output reg read_pixel, // 低电平有效
-    output reg [11:0] pixel_data,
-    output reg h_sync, v_sync 
+
+
+module vga_test_top(
+    input sys_clk,
+    input sys_rst, // 低电平有效
+    output h_sync,
+    output v_sync,
+    output [11:0] pixel_data
 );
-// 行扫描
-reg [9:0] h_count;
-always @(posedge clk) begin
-    if (rst) h_count <= 0;
-    else if (h_count == 799) h_count <= 0;
-    else h_count <= h_count + 1;
-end
-reg [9:0] v_count;
-always @(posedge clk or posedge rst) begin
-    if (rst) v_count <= 0;
-    else if (h_count == 799) begin
-        if (v_count == 524) v_count <= 0;
-        else v_count <= v_count + 1;
-    end
-end
-wire [9:0] row_addr = v_count - 35;
-wire [9:0] col_addr = h_count - 143;
-wire h_s = (h_count > 95);
-wire v_s = (v_count > 1);
-wire rd = (h_count > 142) 
-            && (h_count < 783) 
-            && (v_count > 34) 
-            && (v_count < 515);
-always @(posedge clk ) begin
-    row <= row_addr[8:0];
-    col <= col_addr;
-    read_pixel <= ~rd;
-    h_sync <= h_s;
-    v_sync <= v_s;
-    pixel_data <= read_pixel ? 12'b0 : data_in;
-end
+wire vga_clk;
+wire lock;
+wire rst;
+wire [9:0] x_pos;
+wire [9:0] y_pos;
+wire [11:0] tmp_pixel_data;
+
+test_pic test_pic_inst(
+    .vga_clk(vga_clk),
+    .vga_rst(rst),
+    .x_pos(x_pos),
+    .y_pos(y_pos),
+    .pixel_data(tmp_pixel_data)
+);
+
+assign rst = (lock & sys_rst);
+clkdiv clkdiv_inst(
+    .clk(sys_clk),
+    .rst(~sys_rst),
+    .div_res({29'b0, vga_clk, 2'b0}),
+);
+
+VGA vga_inst(
+    .clk(vga_clk),
+    .rst(rst),
+    .data_in(tmp_pixel_data),
+    .row(y_pos),
+    .col(x_pos),
+    .pixel_data(pixel_data),
+    .h_sync(h_sync),
+    .v_sync(v_sync)
+);
+
 endmodule
+
 
 /*
 set_property PACKAGE_PIN T20 [get_ports {pixel_data[0]}]
